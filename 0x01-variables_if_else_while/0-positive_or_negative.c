be50b9285467955250851bc85033c8a04d796ede
Bassey Ukeme <basseyukemme@gmail.com>
2023-02-17 18:17:12 -0800

    edited file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,12 +3,13 @@
 #include <stdio.h>
 /* more headers goes there */
 
+/* betty style doc for function main goes there */
 /**
- * Determines if a @n is positive, negative or zero.
- * @return Returns zero always.
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
  */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 	int n;
