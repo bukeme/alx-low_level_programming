9b2e25d88a3ef7a92699338f88d120ad415b2906
Bassey Ukeme <basseyukemme@gmail.com>
2023-02-20 10:43:46 -0800

    edited 5-printf.c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,20 +1,12 @@
-/*
- *File: 5-printf.c
- *Auth: Ukeme
- */
-
 #include <stdio.h>
 
-/*
- *Main function
+/**
+ * main -> prints a sentence
  *
- *Prints out a description with printf
- *
- *return 0 at the end
- * /
- 
- int main(void)
- {
-	printf("With proper grammer, but the outcome is a piece of art,\n");
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
- }
+}
