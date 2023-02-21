ef423ac158e6ba8f0c5875fb278606ed393ccd49
Bassey Ukeme <basseyukemme@gmail.com>
2023-02-21 22:12:29 +0100

    added 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * printDesc -> prints _putchar
+ */
+void printDesc() {
+	printf("_putchar\n");
+}
+
+/**
+ * main -> calls printDesc function
+ * 
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printDesc();
+	return 0;
+}
