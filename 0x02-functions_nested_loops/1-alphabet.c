96bb18bb322ab66415e3dd8036ac0138c0ebc833
Bassey Ukeme <basseyukemme@gmail.com>
2023-02-21 23:34:53 +0100

    added 1-alphabet.c file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -> calls print_alphabet
+ * Return: 0 on success
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,3 +4,18 @@
 void printDesc() {
 	printf("_putchar\n");
 }
+
+/**
+ * print_alphabet -> prints alphabets
+ */
+void print_alphabet(void)
+{
+    char alpha = 'a';
+
+    while (alpha <= 'z')
+    {
+        _putchar(alpha);
+        alpha++;
+    }
+    putchar('\n');
+}
