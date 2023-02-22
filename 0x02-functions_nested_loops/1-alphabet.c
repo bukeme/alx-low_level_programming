6473f2e066a4b0858248275f9e7e23c6a6759913
Bassey Ukeme <basseyukemme@gmail.com>
2023-02-22 15:55:41 -0800

    edited 1-alphabet.c file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,10 +8,10 @@ void print_alphabet(void)
 {
 	char alpha = 'a';
 
-    while (alpha <= 'z')
-    {
-        _putchar(alpha);
-        alpha++;
-    }
-    _putchar('\n');
+	while (alpha <= 'z')
+	{
+        	_putchar(alpha);
+        	alpha++;
+	}
+	_putchar('\n');
 }
