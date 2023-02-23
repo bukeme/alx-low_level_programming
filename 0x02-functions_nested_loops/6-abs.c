6f694e662d32f0fe4e0c53e49221931a02c57ad8
Bassey Ukeme <basseyukemme@gmail.com>
2023-02-22 17:33:24 -0800

    added 6-abs.c file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs -> converts number
+ * to its absolute value
+ * @n: number
+ * Return: absolue value of number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	return (n);
+}
+
