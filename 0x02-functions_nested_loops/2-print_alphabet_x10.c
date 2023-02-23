8103d4c6db31e19e742cf79fd128961de3cc852f
Bassey Ukeme <basseyukemme@gmail.com>
2023-02-22 16:38:20 -0800

    added 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.c"
+
+/**
+ * print_alphabet_x10 -> prints alphabets
+ * 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char letter = 'a';
+	int i = 0;
+
+	while (i < 10)
+	{
+		while (letter <= 'z')
+		{
+			_putchar('\n');
+			letter++;
+		}
+		i++;
+	}
+}
+
