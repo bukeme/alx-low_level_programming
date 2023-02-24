e8fe6377218afdf691c5b02584ce14d83f180c33
Bassey Ukeme <basseyukemme@gmail.com>
2023-02-24 13:23:59 -0800

    added 10-print_triangle.c file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle -> prints triangle of
+ * given size
+ * @size: size of triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size -1 - i; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 0; k <= i; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+		_putchar('\n');
+}
+
