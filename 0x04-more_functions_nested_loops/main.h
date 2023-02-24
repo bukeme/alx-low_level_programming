e01517d8c1da357d5a0661b5098edfcb42ef1669
Bassey Ukeme <basseyukemme@gmail.com>
2023-02-24 04:39:45 -0800

    added 3-print_numbers.c file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,7 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#include <stdio.h>
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-
-#endif
