43a49f2c62c162c87535dd9f64cb43b93a77f6fa
Bassey Ukeme <basseyukemme@gmail.com>
2023-02-26 12:42:57 -0800

    added 100-prime_factor.c file

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main -> prints the largest prime factor
+ * of 612852475143
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	long int num = 612852475143;
+	int i = 2;
+
+	while (num != 1)
+	{
+		if (num % i == 0)
+		{
+			num = num / i;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	printf("%d", i);
+	return (0);
+}
