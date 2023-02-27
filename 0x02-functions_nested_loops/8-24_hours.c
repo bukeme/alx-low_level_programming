aa199832413dacb7fa749795415754fb6cb75213
Bassey Ukeme <basseyukemme@gmail.com>
2023-02-26 19:00:12 -0800

    added 8-24_hours.c file

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints hours and minutes
+ */
+void jack_bauer(void)
+{
+	int hour, min;
+	
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hour / 10) + 48);
+			_putchar((hour % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+		}
+	}
+}
+
