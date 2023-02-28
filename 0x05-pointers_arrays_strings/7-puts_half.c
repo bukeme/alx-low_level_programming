9f8497f7f4a0a3dc4b833a1531994d926df7a028
Bassey Ukeme <basseyukemme@gmail.com>
2023-02-28 15:05:11 -0800

    edited 7-puts_half.c file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,7 +10,7 @@ void puts_half(char *str)
 	int i, start, length;
 
 	length = strlen(str);
-	start = length % 2 == 0 ? length / 2 : length - 1 / 2;
+	start = length % 2 == 0 ? length / 2 : (length - 1) / 2;
 	for (i = start; i < length; i++)
 	{
 		_putchar(str[i]);
