72ad5156c92d7868f41daa465624cf685940c666
Bassey Ukeme <basseyukemme@gmail.com>
2023-02-28 14:17:55 -0800

    added 5-rev_string.c file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string -> reverse a string
+ * @s: string
+ */
+
+void rev_string(char *s)
+{
+	int length = strlen(s);
+	int i;
+	char str[100];
+	char temp[2];
+
+	strcpy(str, s);
+	strcpy(s, "");
+	for (i = length - 1; i >= 0; i--)
+	{
+		temp[0] = str[i];
+		temp[1] = '\0';
+		strcat(s, temp);
+	}
+}
+
