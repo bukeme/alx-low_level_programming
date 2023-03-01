9dbdb45616a44913ab849eb8d392d9890f7a0a2c
Bassey Ukeme <basseyukemme@gmail.com>
2023-02-28 16:15:21 -0800

    added 9-strcpy.c file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcpy -> copies string of src to dest
+ * @dest: dest
+ * @src: src
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
+
