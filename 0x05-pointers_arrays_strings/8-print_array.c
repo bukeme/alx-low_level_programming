780e46aae9ca0784c54d0c4d0ddb16917c47a236
Bassey Ukeme <basseyukemme@gmail.com>
2023-02-28 16:02:46 -0800

    added 8-print_array.c file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_number -> prints the integers with putchar
+ * @n: the integer
+ */
+
+void print_number(int n)
+{
+	if (n / 10 != 0)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
+}
+
+/**
+ * print_array -> prints integers in array
+ * @a: array
+ * @n: number of items
+ */
+
+void print_array(int *a, int n)
+{
+	int i, num;
+
+	for (i = 0; i < n; i++)
+	{
+		if (a[i] < 0)
+			_putchar('-');
+
+		num = a[i] < 0 ? a[i] * - 1 : a[i];
+		print_number(num);
+
+		if (i != n - 1)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
+
