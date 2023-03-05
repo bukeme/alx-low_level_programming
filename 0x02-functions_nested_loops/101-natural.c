30e0b1c395b49083d113bcf0d7c8392c930d098c
Bassey Ukeme <basseyukemme@gmail.com>
2023-03-05 05:47:36 -0800

    added 101-natural.c file

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -> prints multiples of 
+ * main -> prints multiples of
  * 3 and 5 that are less than 1024
  * Return: 0 on success
  */
@@ -11,7 +11,7 @@ int main(void)
 	int num = 1024;
 	int i;
 	long int _sum = 0;
-	
+
 	for (i = 3; i < num; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
