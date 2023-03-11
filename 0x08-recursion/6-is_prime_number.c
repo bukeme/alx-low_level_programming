ad20a215935cf8017199445d79448e92dad658c8
Bassey Ukeme <basseyukemme@gmail.com>
2023-03-11 22:01:50 +0100

    added 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,20 +1,46 @@
 #include "main.h"
 
+/**
+ * is_prime -> checks if num is prime
+ * @num: number
+ * @div: divisor
+ * Return: returns 1 if num is prime
+ */
+
 int is_prime(int num, int div)
 {
 	if (num % div == 0)
+	{
 		return (0);
-	if (div < num / 2)
-		is_prime(num, div++);
-	if (div >= num)
+	}
+	else if (div < num / 2)
+	{
+		return (is_prime(num, div++));
+	}
+	if (div >= num / 2)
 		return (1);
+	return (0);
 }
+
+/**
+ * is_prime_number -> returns 1 if n is prime
+ * else return 0
+ * @n: number
+ * Return: 1 if n is prime
+ */
 
 int is_prime_number(int n)
 {
 	if (n <= 1)
+	{
 		return (0);
-	if (n == 2)
+	}
+	else if (n == 2)
+	{
 		return (1);
-	return is_prime(n, 2);
+	}
+	else
+	{
+		return (is_prime(n, 2));
+	}
 }
